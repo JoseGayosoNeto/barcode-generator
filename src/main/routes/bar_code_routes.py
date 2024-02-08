d5999d507576332ba39adb58b62ffe13ca78d892
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-02-08 11:06:41 -0300

    feat: First Implementation of Error Handler

--- src/main/routes/bar_code_routes.py
+++ src/main/routes/bar_code_routes.py
@@ -1,14 +1,19 @@
 from flask import Blueprint, request, jsonify, make_response
 from src.main.views.http_types.http_request import HttpRequest
 from src.main.views.bar_code_creator_view import BarCodeCreatorView
+from src.errors.error_handler import handle_errors
 
 bar_code_blueprints = Blueprint('bar_code_routes', __name__)
 
 @bar_code_blueprints.route('/create_tag', methods=['POST'])
 def create_bar_code():
-    barcode_creator_view = BarCodeCreatorView()
+    response = None
+    try:
+        barcode_creator_view = BarCodeCreatorView()
 
-    http_request = HttpRequest(body=request.json)
-    response = barcode_creator_view.validate_and_create(http_request)
+        http_request = HttpRequest(body=request.json)
+        response = barcode_creator_view.validate_and_create(http_request)
+    except Exception as exception:
+        response = handle_errors(exception)
 
     return make_response(jsonify(response.body), response.status_code)
