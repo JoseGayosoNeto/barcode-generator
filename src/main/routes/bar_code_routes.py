e3dc4305763809c058ff7887c4e16aecbd426181
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-02-08 11:40:52 -0300

    feat: Implementation of Validators and finished Error Handler

--- src/main/routes/bar_code_routes.py
+++ src/main/routes/bar_code_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, make_response
 from src.main.views.http_types.http_request import HttpRequest
 from src.main.views.bar_code_creator_view import BarCodeCreatorView
 from src.errors.error_handler import handle_errors
+from src.validators.bar_code_creator_validator import bar_code_creator_validator
 
 bar_code_blueprints = Blueprint('bar_code_routes', __name__)
 
@@ -9,6 +10,7 @@ bar_code_blueprints = Blueprint('bar_code_routes', __name__)
 def create_bar_code():
     response = None
     try:
+        bar_code_creator_validator(request)
         barcode_creator_view = BarCodeCreatorView()
 
         http_request = HttpRequest(body=request.json)
